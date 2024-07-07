e4525a92130975561e7a3aac1e30a42c8267570b
uniquesonu <21bcs10550@cuchd.in>
2024-07-08 00:58:11 +0530

    refactor: Update About component and add lazy loading for images

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import { twMerge } from "tailwind-merge";
 import { FiArrowRight, FiMail, FiMapPin } from "react-icons/fi";
 import { SiGithub, SiTiktok, SiTwitter, SiYoutube } from "react-icons/si";
@@ -9,7 +9,6 @@ import { Linkedin } from "lucide-react";
 export const About = () => {
   return (
     <div className="min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50">
-      {/* <Logo /> */}
       <motion.div
         initial="initial"
         animate="animate"
@@ -28,9 +27,13 @@ export const About = () => {
   );
 };
 
-const Block = ({ className, ...rest }:any) => {
+const Block = ({ className, children, ...rest }:any) => {
+  const ref = React.useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.5 });
+
   return (
     <motion.div
+      ref={ref}
       variants={{
         initial: {
           scale: 0.5,
@@ -43,6 +46,8 @@ const Block = ({ className, ...rest }:any) => {
           opacity: 1,
         },
       }}
+      initial="initial"
+      animate={isInView ? "animate" : "initial"}
       transition={{
         type: "spring",
         mass: 3,
@@ -54,7 +59,9 @@ const Block = ({ className, ...rest }:any) => {
         className
       )}
       {...rest}
-    />
+    >
+      {children}
+    </motion.div>
   );
 };
 
@@ -185,25 +192,4 @@ const EmailListBlock = () => (
   </Block>
 );
 
-const Logo = () => {
-  // Temp logo from https://logoipsum.com/
-  return (
-    <svg
-      width="40"
-      height="auto"
-      viewBox="0 0 50 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="mx-auto mb-12 fill-zinc-50"
-    >
-      <path
-        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-        stopColor="#000000"
-      ></path>
-      <path
-        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-        stopColor="#000000"
-      ></path>
-    </svg>
-  );
-};
+export default About;
