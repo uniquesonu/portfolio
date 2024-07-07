7f617ab2741eb5cf54a8ddc3e481a4e6e17c68ce
uniquesonu <21bcs10550@cuchd.in>
2024-07-08 00:58:27 +0530

    feat: Update data/index.ts with new projects and work experience

---
+++ data/index.ts
@@ -0,0 +1,70 @@
+
+  export const projects = [
+    {
+      id: 1,
+      title: "Minerva Academy SSB site",
+      des: "Preparatory Academy for Armed Forces & SSB. Minerva Academy specializes in training for SSB Interviews and written entrance exams for various officer-level entries into the Indian Armed Forces.",
+      img: "/ma.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/shadcn.png"],
+      link: "https://minerva-academy.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "Minerva Academy Football Club",
+      des: "MAFC, formerly Minerva Punjab FC, is a professional team from Chandigarh, Punjab, founded in 2014. It focuses on developing football in Punjab, offering local talent a platform and promoting the sport within the community.",
+      img: "/mafc.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/shadcn.png",],
+      link: "https://minerva-academy-fc.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "TerboTrek: The car site",
+      des: "TerboTrek is a user-friendly platform for buying second-hand cars. It allows users to browse listings and schedule test drives at their convenience. Admins can upload cars, manage users, and track status through a comprehensive admin panel.",
+      img: "/carsite.png",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/appwrite.png", "/c.svg"],
+      link: "https://terbotrek.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "CodeConnect: Collaborative Note-Taking & Sharing App",
+      des: "CodeConnect facilitates collaborative note sharing and editing, making it ideal for students, developers, and professionals. Its Markdown support enables users to format notes effectively, incorporating features like headers, lists, links, code snippets, and more, ensuring that notes are not only functional but also visually appealing and easy to navigate.",
+      img: "/codeconnect.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/appwrite.png"],
+      link: "https://codeconnect-ecru.vercel.app/",
+    },
+  ];
+  
+ 
+  
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
