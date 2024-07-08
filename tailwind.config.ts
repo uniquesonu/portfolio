66a450948707c4b6ea91f5d40da8adcdfc15da3a
uniquesonu <21bcs10550@cuchd.in>
2024-07-08 11:56:12 +0530

    feat: Add spotlight animation to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
     extend: {
       animation: {
         shimmer: "shimmer 2s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
         shimmer: {
@@ -25,6 +26,16 @@ const config: Config = {
           },
           to: {
             backgroundPosition: "-200% 0",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
       },
